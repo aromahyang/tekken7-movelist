354315fb245dfaf0eea161c2d265b89ca52065a5
aromahyang <haeng1605@gmail.com>
2021-12-09 22:04:21 +0900

    :lipstick: 캐릭터 카드 UI 만들기

--- src/App.js
+++ src/App.js
@@ -11,27 +11,52 @@ class App {
     this.controlMap;
     this.language;
     this.characters;
-    this.charIndex = 0;
+    this.character = '';
     this.langIndex = 0;
     this.charMenuOpen = false;
+
+    this.$tbodyOfCharacters = document.querySelector(
+      '.character-table > tbody'
+    );
+
     this.mounted();
+    this.addEvent();
     this.render();
   }
 
   mounted() {
     this.controlMap = controlJson;
     this.characters = characterJson;
-    this.language = languageJson[this.characters[this.charIndex].first_name];
+    this.character = this.characters[0].first_name;
+    this.language = languageJson[this.character];
+  }
+
+  addEvent() {
+    this.$tbodyOfCharacters.addEventListener('click', (e) => {
+      const { target } = e;
+      if (!target.dataset.character) {
+        return;
+      }
+
+      const { character } = target.dataset;
+      this.character = character;
+      this.render();
+    });
   }
 
   render() {
     document.body.style.backgroundImage = `url(${bgImg})`;
-    const header = new Header();
-    header.render();
 
+    new Header();
+
+    this.$tbodyOfCharacters.innerHTML = '';
     this.characters.forEach((char, i) => {
-      const card = new CharacterCard({ ...char, index: i });
-      card.render();
+      new CharacterCard({
+        ...char,
+        index: i,
+        selected: this.character === char.first_name,
+        $target: this.$tbodyOfCharacters,
+      });
     });
   }
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,28 +2,40 @@ import thumbnails from '../assets/thumbnails';
 
 class CharacterCard {
   constructor(props) {
-    const { first_name: firstName, last_name: lastName, index } = props;
+    const {
+      first_name: firstName,
+      last_name: lastName,
+      index,
+      selected,
+      $target,
+    } = props;
     this.firstName = firstName;
     this.lastName = lastName;
     this.index = index;
+    this.selected = selected;
+    this.$target = $target;
+    this.render();
   }
 
   template() {
-    return ``;
+    return `
+    <tr>
+      <td class="character-card ${
+        this.selected ? 'character-card--selected' : ''
+      }" data-character="${this.firstName}">
+        <img class="character-card__image" src="${
+          thumbnails[this.firstName]
+        }" data-character="${this.firstName}" />
+        <p class="character-card__name" data-character="${this.firstName}">${
+      this.firstName
+    }</p>
+      </td>
+    </tr>
+    `;
   }
 
   render() {
-    const $tbody = document.querySelector('.character-table > tbody');
-    const $tr = document.createElement('tr');
-    $tr.innerHTML = `
-      <td class="character-card">
-        <img class="character-card__image" src="${
-          thumbnails[this.firstName].default
-        }" />
-        <p class="character-card__name">${this.firstName}</p>
-      </td>
-    `;
-    $tbody.append($tr);
+    this.$target.insertAdjacentHTML('beforeend', this.template());
   }
 }
 
