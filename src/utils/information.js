50b3b75b963970d46481579c57ef72d278636138
aromahyang <haeng1605@gmail.com>
2022-01-26 21:21:37 +0900

    :lipstick: 호밍기, 파워크래시, 스크류 설명 추가

--- src/utils/information.js
+++ src/utils/information.js
@@ -20,20 +20,14 @@ export const INFORMATION = {
     ss : During side step
   `,
   en: `
-    u : ↑,d : ↓,b : ←,f : →
-    u/f : ↗,d/f : ↘,d/b : ↙,u/b : ↖
-    qcf : ↓↘→,qcb : ↓↙←
-    hcf : ←↙↓↘→,hcb : →↘↓↙←
-    PB : Punch Button,KB : Kick Button
     F : Frame,D : Knock Down
     A : Air Combo,B : Block
     J : Just,h : Hold
-    S : Screw,P : Parry
-    H : High,M : Middle,L : Low
-    SP : Special Move,N : Neutrality
-    T : Throw,UB : Unblockable
+    SP : Special Move,
+    UB : Unblockable
     PU : Proximity unblockable
-    WB : Wall Bounce, ws : While standing
+    WB : Wall Bounce, HA : Homing Attack, PC : Power Crush, TS : Screw,
+    ws : While standing
     wc : While crouching, wr : While running
     ss : During side step
   `,
@@ -41,12 +35,12 @@ export const INFORMATION = {
   P : 펀치버튼, K : 킥버튼  LP : 왼손, RP : 오른손  LK : 왼발, RK : 오른발  AP : 왼손 + 오른손  AK : 왼발 + 오른발  AL : 왼손 + 왼발  AR : 오른손 + 오른발  D : 다운, F : 프레임, G : 가드  J : 저스트, H : 홀드, S : 스크류  g : 가드가능, N : 중립, A : 공중콤보  상 : 상단공격, 중 : 중단공격  하 : 하단공격, WB : 월바운드  가불 : 가드불능, 특 : 특수
   `,
   kr: `
-  D : 다운, F : 프레임, G : 가드  J : 저스트, H : 홀드, S : 스크류  g : 가드가능, N : 중립, A : 공중콤보  상 : 상단공격, 중 : 중단공격  하 : 하단공격, WB : 월바운드  가불 : 가드불능, 특 : 특수
+  D : 다운, F : 프레임, G : 가드  J : 저스트, H : 홀드, g : 가드가능, A : 공중콤보 , WB : 월바운드, HA : 호밍기, PC : 파워크래시, TS : 스크류,  가불 : 가드불능, 특 : 특수
   `,
   original_jp: `
   P : パンチボタン、K : キックボタン  LP : 左手, RP : 右手  LK : 左足, RK : 右足  AP : 左手 + 右手  AK : 左足 + 右足  AL : 左手 + 左足  AR : 右手 + 右足  D : ダウン, F : フレーム, G : ガード  J : ジャスト, H : ホールド, S : スクリュー  上 : 上段攻撃, 中 : 中段攻撃  下 : 下段攻撃, N : 中立, A : 空中コンボ  ガ不 : ガード不能, 特 : 特殊  WB : ウォールバウンド
   `,
   jp: `
-  D : ダウン, F : フレーム, G : ガード  J : ジャスト, H : ホールド, S : スクリュー  上 : 上段攻撃, 中 : 中段攻撃  下 : 下段攻撃, N : 中立, A : 空中コンボ  ガ不 : ガード不能, 特 : 特殊  WB : ウォールバウンド
+  D : ダウン, F : フレーム, G : ガード  J : ジャスト, H : ホールド, A : 空中コンボ  ガ不 : ガード不能, 特 : 特殊  WB : ウォールバウンド, HA : ホーミング, PC : パワークラッシュ, TS : スクリュー
   `,
 };
